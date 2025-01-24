23c74b4c09bd4628ef665ba8aba13f0a6b9f707c
3uomlkh <3uomlkh@gmail.com>
2025-01-24 09:56:00 +0900

    init commit

---
+++ src/main/java/com/example/schedulemanagerapi/ScheduleManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulemanagerapi/ScheduleManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
