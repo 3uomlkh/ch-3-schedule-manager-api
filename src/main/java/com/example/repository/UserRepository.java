eeb9ea607b27047648eafc1c757fa6eba0b1dd7b
3uomlkh <3uomlkh@gmail.com>
2025-01-31 17:32:53 +0900

    feat: Add skeleton for UserService and UserRepository interfaces

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.repository;
+
+import com.example.dto.user.UserRequestDto;
+import com.example.dto.user.UserResponseDto;
+import com.example.entity.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    UserResponseDto saveUser(User user);
+
+    UserResponseDto findUserByNameAndPassword(String name, String password);
+
+    List<UserResponseDto> findAllUsers();
+    UserResponseDto findUserByNameAndPassword(User user);
+    UserResponseDto findUserById(Long userId);
+    UserResponseDto updateUser(Long userId, UserRequestDto dto);
+    void deleteUser(Long userId, String password);
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.dto.user.UserRequestDto;
+import com.example.dto.user.UserResponseDto;
+import com.example.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponseDto saveUser(UserRequestDto dto);
+    List<UserResponseDto> findAllUsers();
+    UserResponseDto findUserByNameAndPassword(User user);
+    UserResponseDto findUserById(Long userId);
+    UserResponseDto updateUser(Long userId, UserRequestDto dto);
+    void deleteUser(Long userId, String password);
+}
